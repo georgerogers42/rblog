31f95f2a3ca46e3a8ddbbf67226ff7297abd1d26
George Rogers <geo@bob.localhost>
2015-05-22 11:36:52 -0500

    update

--- rblog.go
+++ rblog.go
@@ -1,6 +1,15 @@
 package rblog
 import (
+	"net/http"
 	"github.com/gorilla/mux"
 )
 
 var App = mux.NewRouter()
+
+func init() {
+	App.HandleFunc("/", Index)
+	App.HandleFunc("/{slug}", Slug)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {}
+func Slug(w http.ResponseWriter, r *http.Request) {}
