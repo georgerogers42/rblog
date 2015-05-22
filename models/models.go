a2520b95fd760f5a7e45c160acabfb660d9e2956
George Rogers <geo@bob.localhost>
2015-05-22 12:35:29 -0500

    update

--- models/models.go
+++ models/models.go
@@ -17,17 +17,19 @@ type Article struct {
 	Posted, Updated time.Time
 }
 
-func AllArticles(db *sql.DB) ([]Article, error) {
+func AllArticles(db *sql.DB) ([]*Article, error) {
 	rows, err := db.Query("SELECT author.id, pseudonym, name, encpass, article.id, slug, title, contents, posted, updated FROM author, article WHERE author.id = article.fk_author ORDER BY posted DESC")
-	if err != err {
+	if err != nil {
 		return nil, err
 	}
 	ret := []Article{}
 	for rows.Next() {
 		v := Article{}
-		rows.Scan(&v.Author.Id, &v.Author.Pseudonym, &v.Author.Name, &v.Author.EncPass)
-		rows.Scan(&v.Slug, &v.Title, &v.Posted, &v.Updated)
-		ret = append(ret, v)
+		rows.Scan(
+			&v.Author.Id, &v.Author.Pseudonym, &v.Author.Name, &v.Author.EncPass,
+			&v.Id, &v.Slug, &v.Title, &v.Posted, &v.Updated,
+		)
+		ret = append(ret, &v)
 	}
 	if err = rows.Err(); err != nil {
 		return nil, err
